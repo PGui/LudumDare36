1a05ffd6c24a9b5bf06aa8bc3a2e472ffd02c61b
Guillaume Pastor <guillaume.pastor@dont-nod.com>
2016-08-27 16:29:44 +0200

    Camera Shake !

--- LudumDare36/Assets/Scripts/CharacterAction.cs
+++ LudumDare36/Assets/Scripts/CharacterAction.cs
@@ -8,11 +8,20 @@ public class CharacterAction : MonoBehaviour {
     public float Force = 100.0f;
     public float CooldownDuration = 0.2f;
     private bool CanShoot = true;
-
+    public GameObject Camera;
+    private ScreenshakeMgr SSMgr;
 
 	// Use this for initialization
 	void Start () {
         ShootDirection = new Vector2(1.0f, 0.0f);
+        if(Camera)
+        {
+            SSMgr = Camera.GetComponent<ScreenshakeMgr>();
+            if(!SSMgr)
+            {
+                Debug.Log("Error Getting SSMGR");
+            }
+        }
     }
 	
 	// Update is called once per frame
@@ -25,6 +34,7 @@ public class CharacterAction : MonoBehaviour {
             ShootDirection = ShootDirection.normalized;
             GameObject Projectile = GameObject.Instantiate(ObjectToShoot, this.transform.position, Quaternion.identity) as GameObject;
             Projectile.GetComponent<Rigidbody2D>().AddForce(ShootDirection * Force, ForceMode2D.Impulse);
+            SSMgr.StartShake(0.3f, 0.8f, 8.0f);
 
         }
     }
