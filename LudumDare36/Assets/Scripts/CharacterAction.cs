465ec6aab59276aceac34a437bd8dc2d8358f23b
Guillaume Pastor <guillaume.pastor@dont-nod.com>
2016-08-27 13:35:17 +0200

    Shooting Nokias !

---
+++ LudumDare36/Assets/Scripts/CharacterAction.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterAction : MonoBehaviour {
+
+    public GameObject ObjectToShoot;
+    private Vector2 ShootDirection;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if(Input.GetButtonDown("Fire1"))
+        {
+            ShootDirection = Vector3.Normalize((Input.mousePosition - this.transform.position));
+
+            GameObject Projectile = GameObject.Instantiate(ObjectToShoot, this.transform.position, Quaternion.identity) as GameObject;
+
+            Projectile.GetComponent<Rigidbody2D>().AddForce(ShootDirection * 100, ForceMode2D.Impulse);
+
+        }
+        
+
+    }
+}
--- LudumDare36/Assets/Scripts/CharacterMovement.cs
+++ LudumDare36/Assets/Scripts/CharacterMovement.cs
@@ -3,13 +3,47 @@ using System.Collections;
 
 public class CharacterMovement : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+
+
+    public float Speed = 20.0f;
+    public float Inertia = 2.5f;
+    public Vector2 MaxVelocity = new Vector2(100,100);
+    public AnimationCurve AccelerationCurve;
+    public AnimationCurve DecelerationCurve;
+    // Use this for initialization
+    void Start () {
 	
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
-	}
+	void Update ()
+    {
+        float HorizontalAxis = Input.GetAxis("Horizontal");
+        float VerticalAxis = Input.GetAxis("Vertical");
+
+        float AccelerationX = 0.0f;
+        float AccelerationY = AccelerationCurve.Evaluate(VerticalAxis);
+
+
+        if (HorizontalAxis > 0.1f)
+        {
+            AccelerationX = AccelerationCurve.Evaluate(HorizontalAxis);
+        }
+        else if (HorizontalAxis < -0.1f)
+        {
+            AccelerationX = -1.0f * AccelerationCurve.Evaluate(-1.0f * HorizontalAxis);
+        }
+
+        
+        if (VerticalAxis > 0.1f)
+        {
+            AccelerationY = AccelerationCurve.Evaluate(VerticalAxis);
+        }
+        else if (VerticalAxis < -0.1f)
+        {
+            AccelerationY = -1.0f * AccelerationCurve.Evaluate(-1.0f * VerticalAxis);
+        }
+
+        this.transform.position = new Vector3(transform.position.x + AccelerationX * Speed * Time.deltaTime, transform.position.y + Speed * AccelerationY * Time.deltaTime, this.transform.position.z);
+    }
 }
